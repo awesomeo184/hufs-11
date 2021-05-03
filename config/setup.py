881a6656dbf80fcb1df9ce8c471b371d035dfe68
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-05-03 16:33:06 +0900

    edit: add resource path

---
+++ config/setup.py
@@ -0,0 +1,8 @@
+import os
+import setuptools
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+RESOURCES_DIR = os.path.join(BASE_DIR, "resources")
+UI_PATH = os.path.join(RESOURCES_DIR, "ui")
+MODEL_PATH = os.path.join(RESOURCES_DIR, "models")
