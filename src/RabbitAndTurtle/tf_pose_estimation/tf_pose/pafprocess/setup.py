a17821f4f5e881392b53128ba68b66a7bef1d1be
siwan213 <80950425+siwan213@users.noreply.github.com>
2021-06-01 23:15:05 +0900

    setup.py 파일 수정
    setup.py 파일 수정입니다.
    pafprocess 경로로 들어가서 다시 python setup.py install
    해주세요.

--- src/RabbitAndTurtle/tf_pose_estimation/tf_pose/pafprocess/setup.py
+++ src/RabbitAndTurtle/tf_pose_estimation/tf_pose/pafprocess/setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup, Extension
 import numpy
 import os
+import numpy as np
 
 # os.environ['CC'] = 'g++';
 setup(name='pafprocess_ext', version='1.0',
@@ -14,3 +15,11 @@ setup(name='pafprocess_ext', version='1.0',
         "pafprocess"
     ]
 )
+
+EXT = Extension('_pafprocess',
+                sources=[
+                    'tf_pose/pafprocess/pafprocess_wrap.cpp',
+                    'tf_pose/pafprocess/pafprocess.cpp',
+                ],
+                swig_opts=['-c++'],
+                include_dirs=[np.get_include()])
