d884507d508c17629679a3c71f327fdfb82f2e4f
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-06-02 20:20:17 +0900

    feat: 사용자 통계 - 사용시간, 눈 깜빡임 경고 횟수 출력

--- src/RabbitAndTurtle/views.py
+++ src/RabbitAndTurtle/views.py
@@ -2,254 +2,116 @@ import os
 import sys
 import time
 
-import cv2
-
-from PyQt5.QtCore import QThread, pyqtSignal, Qt, pyqtSlot
-from PyQt5.QtGui import QImage, QPixmap
-
-from config import setup
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
+from PyQt5 import QtCore, QtGui, uic
+from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-from src.RabbitAndTurtle.EyeTracker import EyeTracker
-from src.RabbitAndTurtle.NeckTracker import NeckTracker
+from config import setup
+from EyeTracker import EyeTracker
+from signals import DriedEye, ImageThread
 
 PATH = setup.UI_PATH
-
 start_form = uic.loadUiType(os.path.join(PATH, "StartWindow.ui"))[0]
 exec_form = uic.loadUiType(os.path.join(PATH, "ExecWindow.ui"))[0]
-setting_form = uic.loadUiType(os.path.join(PATH, "SettingWindow.ui"))[0]
 status_form = uic.loadUiType(os.path.join(PATH, "StatusWindow.ui"))[0]
-alarm_form = uic.loadUiType(os.path.join(PATH, "AlarmWindow.ui"))[0]
 
-eyeTracker = None
-
-# TODO: 예외 처리, 팝업창, 모듈 연결, 디자인
 
 class StartWindow(QMainWindow, start_form):
-    '''
-    시작창
-
-    오브젝트 목록
-    - 타이틀: label_title
-    - 시작버튼: btn_start
-    - 설정버튼: btn_setting
-    '''
-
     def __init__(self):
         super(StartWindow, self).__init__()
         self.setupUi(self)
-        self.setting_window = SettingWindow()
 
-        self.btn_start.clicked.connect(self.activate_exec_window)
-        self.btn_setting.clicked.connect(self.setting_window.show)
-        self.show()
+        self.btn_start.clicked.connect(self.goto_exec_window)
 
-    def activate_exec_window(self):
-        self.hide()
-        self.exec_window = ExecWindow(self)
-        self.exec_window.show()
-
-
-
-class ImageThread(QThread):
-    def __init__(self, dried_eye_signal):
-        super().__init__()
-        self.is_interrupted = False
-        self.dried_eye_signal = dried_eye_signal
-
-    changePixmap = pyqtSignal(QImage)
-
-    def run(self):
-        global eyeTracker
-        cap = cv2.VideoCapture(0)
-        eyeTracker = EyeTracker()
-        neckTracker = NeckTracker()
-
-        count = eyeTracker.warning_count
-
-        while not self.is_interrupted:
-            ret, frame = cap.read()
-
-            if ret:
-                rgbImage = eyeTracker.is_blinked(frame)
-                neckTracker.is_good_posture(rgbImage)
-
-                if count != eyeTracker.warning_count:
-                    self.dried_eye_signal.dry()
-                    count = eyeTracker.warning_count
-
-                h, w, ch = rgbImage.shape
-                bytesPerLine = ch * w
-                convertToQtFormat = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
-                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
-                self.changePixmap.emit(p)
+    def goto_exec_window(self):
+        execWindow = ExecWindow()
+        widgets.addWidget(execWindow)
+        widgets.setCurrentWidget(execWindow)
 
 
 class ExecWindow(QMainWindow, exec_form):
-    '''
-    실행창
 
-    오브젝트 목록
-    - 카메라 온/오프 버튼: btn_camera
-    - 웹캠 화면: widget_webcam
-    - 종료 버튼(통계 창으로 이동): btn_status
-    - 설정 버튼: btn_setting
-    '''
-
-    @pyqtSlot(QImage)
-    def __init__(self, parent):
-        super(ExecWindow, self).__init__(parent)
+    def __init__(self):
+        super(ExecWindow, self).__init__()
         self.setupUi(self)
-        self.setting_window = parent.setting_window
-        self.dried_eye_signal = DriedEye()
 
-        self.btn_status.clicked.connect(self.terminate)
-        self.btn_setting.clicked.connect(self.setting_window.show)
-        self.dried_eye_signal.dried.connect(self.show_eye_dried_pop_up_message)
+        # * utility
+        self.eye_warning_signal = DriedEye()
+        self.eyeTracker = EyeTracker()
+        self.thread = ImageThread(self.eyeTracker, self.eye_warning_signal)
+        self.timer = QTimer()
+        self.time_count = 0
+        self.timer.setInterval(1000)
 
-        self.thread = ImageThread(self.dried_eye_signal)
-        self.thread.changePixmap.connect(self.setImage)
+        # * click signal
+        self.btn_status.clicked.connect(self.goto_status_window)
+        self.timer.timeout.connect(self.print_time)
+
+        self.label_time.setText("00:00:00")
+
+        # * custom signal
+        self.eye_warning_signal.dried.connect(self.show_eye_warning_message)
+        self.thread.stream.connect(self.set_image)
+
+        self.timer.start()
         self.thread.start()
 
-        self.count = 0
-        self.flag = True
+    def print_time(self):
+        self.time_count += 1
+        strftime = time.strftime('%H:%M:%S', time.gmtime(self.time_count))
+        self.label_time.setText(strftime)
 
-        # setting text to the label
-        self.label_time.setText(str(self.count))
+    def goto_status_window(self):
+        statusWindow = StatusWindow(self.time_count, 0, self.eyeTracker.get_warning_count())
+        self.thread.is_interrupted = True
+        widgets.addWidget(statusWindow)
+        widgets.setCurrentWidget(statusWindow)
 
-        # creating a timer object
-        timer = QTimer(self)
-
-        # adding action to timer
-        timer.timeout.connect(self.showTime)
-
-        # update the timer every tenth second
-        timer.start(100)
-
-    def show_eye_dried_pop_up_message(self):
+    @pyqtSlot(int)
+    def show_eye_warning_message(self, warning_count):
+        print("Warning count = ", warning_count)
         box = QMessageBox()
         box.setWindowTitle("eye dry warning")
         box.setText("안구 건조 위험")
         box.resize(300, 200)
         box.exec_()
 
-
-    def showTime(self):
-        # checking if flag is true
-        if self.flag:
-            # incrementing the counter
-            self.count += 1
-
-        # getting text from count
-        strftime = time.strftime('%H:%M:%S', time.gmtime(self.count))
-        text = str(strftime)
-
-        # showing text
-        self.label_time.setText(text)
-
-    def setImage(self, image):
+    @pyqtSlot(QImage)
+    def set_image(self, image):
         self.label_webcam.setPixmap(QPixmap.fromImage(image))
-
-    def terminate(self):
-        self.thread.is_interrupted = True
-        self.hide()
-        self.status_window = StatusWindow(self)
-        self.status_window.show()
-
-
-# 안구건조 시그널
-class DriedEye(QObject):
-
-    dried = pyqtSignal()
-
-    def __init__(self):
-        super().__init__()
-
-    def dry(self):
-        self.dried.emit()
-
-class SettingWindow(QMainWindow, setting_form):
-    '''
-    오브젝트 목록
-    - 거북목 알림 : check_box_turtle_neck
-    - 안구 건조 알림: check_box_eye_track
-    - 소리 알림: check_box_sound
-    - 방해 금지 모드: check_box_no_disturb
-    - 완료 버튼: btn_save
-    '''
-
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-
-        self.check_box_turtle_neck.setChecked(True)
-        self.check_box_eye_track.setChecked(True)
-        self.check_box_sound.setChecked(True)
-
-        self.btn_save.clicked.connect(self.save_states)
-
-    def save_states(self):
-        # TODO: 설정에 맞춰 기능 활성화
-        if self.check_box_turtle_neck.isChecked():
-            pass
-        if self.check_box_eye_track.isChecked():
-            pass
-        if self.check_box_sound.isChecked():
-            pass
-        if self.check_box_no_disturb.isChecked():
-            pass
-        self.close()
 
 
 class StatusWindow(QMainWindow, status_form):
-    '''
-    오브젝트 목록
-    - 사용시간 : label_time_count
-    - 거북목 경고 횟수: label_turtle_count
-    - 눈 깜빡임 경고 횟수: label_eye_count
-    - 확인 버튼: btn_exit
-    '''
-
-    def __init__(self, parent):
-        super(StatusWindow, self).__init__(parent)
+    def __init__(self, exec_time, turtle_neck_warning_count, eye_dried_warning_count):
+        super(StatusWindow, self).__init__()
         self.setupUi(self)
-        self.get_status()
+        self.exec_time = exec_time
+        self.turtle_neck_warning_count = turtle_neck_warning_count
+        self.eye_dried_warning_count = eye_dried_warning_count
+
+        # * labels
+        self.label_time_count.setText(time.strftime('%H:%M:%S', time.gmtime(self.exec_time)))
+        self.label_turtle_count.setText(str(self.turtle_neck_warning_count))
+        self.label_eye_count.setText(str(self.eye_dried_warning_count))
+
         self.btn_exit.clicked.connect(self.exit)
 
-    def get_status(self):
-        # TODO: 통계 모듈 연결
-        self.label_time_count.setText("08:11:11")
-        self.label_turtle_count.setText("14")
-        self.label_eye_count.setText("5")
-
     def exit(self):
-        self.close()
+        # TODO: 안전 종료
+        App.quit()
 
-
-class AlarmWindow(QMainWindow, alarm_form):
-    '''
-    오브젝트 목록
-    - 눈 깜빡임 횟수: label_blink_count
-    - 확인 버튼: btn_exit
-    '''
-
-    def __init__(self, parent):
-        super(AlarmWindow, self).__init__(parent)
-        self.setupUi(self)
-        self.get_status()
-        self.btn_exit.clicked.connect(self.exit)
-
-    def get_status(self):
-        # TODO: 깜빡임 횟수 연결
-        self.label_blink_count("8")
-
-    def exit(self):
-        self.close()
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    myWindow = StartWindow()
-    app.exec_()
+    App = QApplication(sys.argv)
+
+    widgets = QStackedWidget()
+    startWindow = StartWindow()
+
+    widgets.addWidget(startWindow)
+
+    widgets.setMinimumWidth(800)
+    widgets.setMinimumHeight(600)
+    widgets.show()
+    App.exec_()
